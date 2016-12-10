e65515e15a040f7a96c74eea30b1e1f1d425d9a2
Mary Willis <msibilsk@gmail.com>
2016-12-10 10:55:35 -0600

    initial files

---
+++ assets/app.js
@@ -0,0 +1,13 @@
+// Initialize Firebase
+var config = {
+apiKey: "AIzaSyBK8x0wgYA9glOk4f0KzKQ1u6s8--9p7XE",
+authDomain: "employee-data-management-7ee13.firebaseapp.com",
+databaseURL: "https://employee-data-management-7ee13.firebaseio.com",
+storageBucket: "employee-data-management-7ee13.appspot.com",
+messagingSenderId: "178524481500"
+};
+firebase.initializeApp(config);
+
+database.ref().set({
+	
+});
